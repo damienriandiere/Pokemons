016c191b8b9bfe9a940e17539507fb42e473a12c
Kerpo <damien.riandiere@outlook.fr>
2023-11-17 10:32:42 +0100

    Add gitignore

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 const dotenv = require('dotenv');
 const SwaggerUI = require('swagger-ui-express');
 const swaggerJsdoc = require('swagger-jsdoc');
+import swaggerOptions from './models/swaggerOptions.json';
 import AuthController from './api/auth';
 import PokemonController from './api/pokemon';
 import { connectDB } from './loaders/mongoose';
@@ -19,19 +20,6 @@ app.use(bodyParser.json());
 app.use(helmet());
 app.use(AuthController);
 app.use(PokemonController);
-
-// Set up Swagger options
-const swaggerOptions = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Your API Documentation',
-      version: '1.0.0',
-      description: 'Description of your API',
-    },
-  },
-  apis: ['./api/*.js'], // specify the path to your API routes
-};
 
 // Generate Swagger documentation
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
