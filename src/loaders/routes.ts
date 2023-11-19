79ca42bcc399fbcdb5d8264d26dc25bc8e8da789
Kerpo <damien.riandiere@outlook.fr>
2023-11-19 11:59:40 +0100

    Update swagger

--- src/loaders/routes.ts
+++ src/loaders/routes.ts
@@ -1,6 +1,5 @@
 import { Express } from "express";
 import swaggerUi from 'swagger-ui-express';
-import swaggerJSDoc from 'swagger-jsdoc';
 import AuthController from '../api/auth';
 import PokemonController from '../api/pokemon';
 import swaggerOptions from '../static/swaggerOptions.json';
@@ -8,9 +7,7 @@ import swaggerOptions from '../static/swaggerOptions.json';
 export default (app: Express) => {
     console.log("Registering routes");
 
-    const swaggerSpec = swaggerJSDoc(swaggerOptions);
-    
     app.use(AuthController);
     app.use(PokemonController);
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOptions));
 }
